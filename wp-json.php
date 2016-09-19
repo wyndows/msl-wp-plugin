da15b38410d892dca561a5d4730877d2a06be97b
Marlan Ball <wyndows@users.noreply.github.com>
2016-09-19 12:37:07 -0600

    Add files via upload

--- wp-json.php
+++ wp-json.php
@@ -8,6 +8,13 @@
   License: GPLv2+
 */
 
+// to utilize the style sheet on the admin page
+function wpj_load_plugin_css() {
+	$plugin_url = plugin_dir_url( __FILE__ );
+	wp_enqueue_style( 'style1', $plugin_url . 'css/style.css' );
+}
+add_action( 'admin_enqueue_scripts', 'wpj_load_plugin_css' );
+
 
 class WP_Json {
 
@@ -39,6 +46,7 @@ class WP_Json {
 		// starting session if not already started so data can be used by different pages
 		if ( ! session_id() ) {
 			session_start();
+
 		}
 	}
 
@@ -46,39 +54,11 @@ class WP_Json {
 	* Actions performed at the loading of menu pages
 	*/
 
-
-	// displays the output from the Mindshare API using settings from wpj-settings() and the 'display' shortcode.
+	// displays the output from the Mindshare API using settings from wpj-settings() and the 'display_the_data' shortcode.
 	public static function wpj_display() {
 
-		// checking if user has submitted the settings page and if not sends user to settings page.  prevents an error if user didn't go there first.
-		if ( !isset( $_SESSION['number_of_posts'] ) ) {
-			WP_Json::wpj_settings();
-			die;
-		} else {
-			$numPosts = $_SESSION['number_of_posts'];
-		}
+		echo (do_shortcode( '[display_the_data]' ));
 
-		//displays the category of posts at the top of the display page from shortcode 'display'
-		echo '<p style="color: blue; font-size: 24px; text-transform: uppercase">' . do_shortcode( '[display]' ) . '</p>';
-
-		// iterating through the number of posts desired to be displayed
-		for ( $n = 1; $n <= $numPosts; $n ++ ) {
-
-			// checking if the amount of desired posts is greater than the actual available and stopping the display if it is
-			if ( $n > count( $_SESSION['title'] ) ) {
-				die;
-			}
-
-			// setting the date value to just year/month/day
-			$date = substr( ( $_SESSION['date'][ $n - 1 ] ), 0, 10 );
-
-			// displaying the values that were added to the session array in the shortcode 'display', with some css styling
-			echo '<img src=https://gallery.mailchimp.com/bf1d0f5bebb27fa15f1c84adb/images/78e77fb1-064b-47bf-96ea-43b234357c2c.png height=50 width=50>';
-			echo '<p style="color: orangered; font-size: 18px; width: 66%;">' . $_SESSION['title'][ $n - 1 ] . '</p>';
-			echo '<p style="color: black; font-size: 13px;">' . "by: " . $_SESSION['author'][ $n - 1 ] . "&nbsp;&nbsp;&nbsp;&nbsp;" . $date . '</p>';
-			echo '<div style="color:black; font-size:13px; width: 66%;">' . $_SESSION['content'][ $n - 1 ] . '-------------------------------------------------------------' . '</div>';
-			echo '<br><br><br><br><br>';
-		}
 	}
 
 	/*
@@ -95,41 +75,7 @@ class WP_Json {
 		}
 
 		// the form for entering in settings used in the shortcode 'display' as well as the Display page
-
-		echo "<br><p><span style=\"font-size: 15px; color: blue\">WP Json Settings Page</span></p><p>After making your choices, please click <span style=\"font-size: 15px;font-weight: bold\">Submit</span>, then wait until the settings page resets to '1' and 'News' respectively (1-2 seconds).  Click on <span style=\"font-size: 15px;font-weight: bold\">Display</span> under <span style=\"font-size: 15px;font-weight: bold\">WP Json</span> plugin name to view chosen posts.</p>
-
-<!-- The form for setting user choices -->
-<form  action='' method='post'>
-    <table width='450px'>
-        <tr>
-            <td valign='top'>
-                <label for='number of posts'>Number of Posts </label>
-            </td>
-            <td valign='top'>
-                <input  type='number' name='number_of_posts' value='1' min='1' max='$posts' size='1'>
-            </td>
-        </tr>
-
-        <tr>
-            <td valign='top'>
-                <label for='post category'>Post Category </label>
-            </td>
-            <td valign='top'>
-				<select name='post_category'>
-					<option value='News'>News</option>
-					<option value='Code'>Code</option>
-					<option value='Downloads'>Downloads</option>
-				</select>
-            </td>
-        </tr>
-
-		<tr>
-            <td valign='top'>
-            	<input  type='submit' value='Submit'>            	
-            </td>
-        </tr>
-    </table>
-	</form>";
+		include( 'views/display-form.php' );
 
 		// making sure the POST went through and then moving the values to the SESSION cookie on the server so it can be used by other pages
 		if ( isset ( $_POST['post_category'] ) ) {
@@ -154,7 +100,7 @@ class WP_Json {
 	/*
 	 * Setting up the shortcode, this gets the data from the API and fills $_SESSION with results so they can be utilized in the wpj-display function
 	 */
-	public static function what_display() {
+	public static function create_display() {
 
 		// checking if the settings page has been submitted, if so setting category and number, and in case it hasn't a default setting
 		if ( isset( $_SESSION['post_category'] ) && ( $_SESSION['number_of_posts'] ) ) {
@@ -206,10 +152,50 @@ class WP_Json {
 		return $postCategory;
 	}
 
+	public static function display_the_data() {
+		// starting the session so we can use the data from the settings
+		if ( ! session_id() ) {
+			session_start();
+		}
+
+		// utilizing the style.css file for styling the output
+		$plugin_url = plugin_dir_url( __FILE__ );
+		wp_enqueue_style( 'style1', $plugin_url . 'css/style.css' );
+
+
+		// checking if user has submitted the settings page and if not sends user to settings page.  prevents an error if user didn't go there first.
+		if ( ! isset( $_SESSION['number_of_posts'] ) ) {
+			WP_Json::wpj_settings();
+			die;
+		} else {
+			$numPosts = $_SESSION['number_of_posts'];
+		}
+
+		//displays the category of posts at the top of the display page from shortcode 'display'
+		include ('views/display-category.php');
+
+		// iterating through the number of posts desired to be displayed
+		for ( $n = 1; $n <= $numPosts; $n ++ ) {
+
+			// checking if the amount of desired posts is greater than the actual available and stopping the display if it is
+			if ( $n > count( $_SESSION['title'] ) ) {
+				die;
+			}
+
+			// setting the date value to just year/month/day
+			$date = substr( ( $_SESSION['date'][ $n - 1 ] ), 0, 10 );
+
+			include ('views/display-posts.php');
+		}
+
+
+	}
+
 }
 
 // wordpress shortcode method for a class
-add_shortcode( 'display', array( 'WP_Json', 'what_display' ) );
+add_shortcode( 'create_display', array( 'WP_Json', 'create_display' ) );
+add_shortcode( 'display_the_data', array( 'WP_Json', 'display_the_data' ) );
 
 // instantiate a new WP_Json object so the pages can be loaded
 new WP_Json();
